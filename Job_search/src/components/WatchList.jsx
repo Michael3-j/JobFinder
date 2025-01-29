5d796290be3f91115ba9280f72b4e27a51dd8397
eddy <eddy.mwaniki.27@gmail.com>
2025-01-29 13:48:24 +0300

    modifies index.ss

---
+++ Job_search/src/components/WatchList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function WatchList(){
+   return(
+    <div>
+        <button>✖</button>
+        <button>Apply💼</button>
+    </div>
+   )
+}
+
+export default WatchList;
