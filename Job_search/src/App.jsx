a1d139628e993f006b803ccf16ee7add6ebb859d
DerrickDipondo <derrickdipondo@gmail.com>
2025-01-30 17:24:39 +0300

    Render the Application Form

--- Job_search/src/App.jsx
+++ Job_search/src/App.jsx
@@ -3,12 +3,26 @@ import WatchList from "./components/WatchList";
 import ApplicationForm from "./components/ApplicationForm"
 import { useState } from "react";
 
+
+
 function App() {
   const [watchList, setWatchList] = useState([])
+
+  //The logic for storing submitted applications
+  const [applications, setApplications] = useState([]);
+
+  //This will enable us to submit the form
+  const handleFormSubmit = (formData) => {
+    setApplications([...applications, formData]);
+  };
+
+
+
   return ( 
     <>
      <WatchList watchList={watchList} setWatchList={setWatchList}/>
      <JobList setWatchList={setWatchList}/>
+     <ApplicationForm submitForm={handleFormSubmit} />
     </>
   )
 }
--- Job_search/src/components/ApplicationForm.jsx
+++ Job_search/src/components/ApplicationForm.jsx
@@ -3,7 +3,8 @@ import React, { useState } from "react";
 
  function ApplicationForm({ submitForm }) {
     const [ formData, setFormData] = useState({
-        name: "",
+        firstName: "",
+        secondName: ""
         email: "",
     })
 
@@ -26,13 +27,38 @@ import React, { useState } from "react";
     return (
         <div>
             <h2>Application Form<h2>
+                <form onSumit={handleSubmit}>
+                    <label>First Name: </label>
+                    <input 
+                      type="text"
+                      name="firstName"
+                      value={formData.name}
+                      onChange={handleChange}
+                    />
 
+                    <br />
 
+                    <label>Second Name</label>
+                    <input
+                      type="text"
+                      name="secondName"
+                      value={formData}
+                      onChange={handleChange}
 
+                    <label> Email: </label>
+                    <input
+                      type="email"
+                      name="email"
+                      value={formData.email}
+                      onChange={handleChange}
+                    />
+
+                    <br />
+
+                    <button type="submit">Submit</button>
+                </form>
         </div>
-
     )
-    
  }
 
  export default ApplicationForm
