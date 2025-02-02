b143c805209db3b2d7bea2b3c958d7ea20fac8a5
eddy <eddy.mwaniki.27@gmail.com>
2025-02-02 20:27:48 +0300

    more classnames

--- Job_search/src/Component/NavBar.jsx
+++ Job_search/src/Component/NavBar.jsx
@@ -1,7 +1,7 @@
 import { NavLink } from "react-router-dom"
 export default function NavBar () {
     return (
-        <nav>
+        <nav className="navbar" >
             <NavLink to="/">
                 Home
             </NavLink>
