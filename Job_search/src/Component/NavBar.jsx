039e56c7f597183867e5eecf5f1d4331fd4e4e11
Michael <michael.njoroge3@student.moringaschool.com>
2025-02-03 08:17:20 +0300

    changes

--- Job_search/src/Component/NavBar.jsx
+++ Job_search/src/Component/NavBar.jsx
@@ -1,13 +1,8 @@
 import { NavLink } from "react-router-dom"
 export default function NavBar () {
     return (
-<<<<<<< HEAD
         <nav className="navbar">
             <NavLink  to="/">
-=======
-        <nav  >
-            <NavLink to="/">
->>>>>>> 910c6121e2c1eb5da6c761c2be2f0b71c955d0b7
                 Home
             </NavLink>
             <NavLink to="/watch-list">
